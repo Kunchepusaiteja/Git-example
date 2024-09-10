d9cffc08ed17de14684e6a0f5d2a2962bebba2c4
Kunchepusaiteja <kunchepusaiteja710@gmail.com>
2024-09-10 15:16:58 +0530

    first commit

--- Code.java
+++ Code.java
@@ -2,5 +2,6 @@ class Code{
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("First revert");
+        System.out.println(10+20+30);
     }
 }
